0f4c5e58c901d8f619ec68982f6c088414a8eacc
tbs.lmaa <tbs.lmaa@fb4725b4-b61b-11dd-a80a-757f205f25d6>
2008-11-19 10:14:40 +0000

    Initial Import

---
+++ trunk/model/src/main/java/de/fh_fortmund/cw/kniffel/model/App.java
@@ -0,0 +1,13 @@
+package de.fh_fortmund.cw.kniffel.model;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
