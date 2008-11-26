b924098675697e4d9bd66bd018a92796c8eca3c6
tbs.lmaa <tbs.lmaa@fb4725b4-b61b-11dd-a80a-757f205f25d6>
2008-11-26 09:46:25 +0000

    Methode erstelleNeuesSpiel(int) implementiert

--- service/src/main/java/de/fh_fortmund/cw/kniffel/ejb3/service/KniffelSteuerungImpl.java
+++ service/src/main/java/de/fh_fortmund/cw/kniffel/ejb3/service/KniffelSteuerungImpl.java
@@ -1,15 +1,30 @@
 package de.fh_fortmund.cw.kniffel.ejb3.service;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.ejb.Stateful;
 
 import de.fh_fortmund.cw.kniffel.model.KniffelZettel;
+import de.fh_fortmund.cw.kniffel.model.Spieler;
 
 @Stateful
 public class KniffelSteuerungImpl implements KniffelSteuerung {
 
+	/*
+	 * private static final Log logger = LogFactory
+	 * .getLog(KniffelSteuerungImpl.class);
+	 */
+
+	/**
+	 * Das aktuelle Spiel
+	 */
+	private KniffelZettel spiel;
+
 	public KniffelZettel erstelleNeuesSpiel(int spielerAnzahl) {
-		// TODO Auto-generated method stub
-		return null;
+		List<Spieler> spielerList = new ArrayList<Spieler>(spielerAnzahl);
+		this.spiel = new KniffelZettel(spielerList);
+		return this.spiel;
 	}
 
 	public KniffelZettel getKniffelZettel() {
@@ -19,67 +34,67 @@ public class KniffelSteuerungImpl implements KniffelSteuerung {
 
 	public void set1er() {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	public void set2er() {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	public void set3er() {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	public void set4er() {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	public void set5er() {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	public void set6er() {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	public void setChance() {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	public void setDreierPasch() {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	public void setFullHouse() {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	public void setGrosseStrasse() {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	public void setKleineStrasse() {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	public void setKniffel() {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	public void setViererPasch() {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 }
