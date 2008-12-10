bd80aa2b98fe39db08f766a6c6022d95ef353905
tbs.lmaa <tbs.lmaa@fb4725b4-b61b-11dd-a80a-757f205f25d6>
2008-12-10 11:45:15 +0000

    So, dass man mal was sieht und passiert ;)

---
+++ webclient-gwt/src/main/java/de/fh_dortmund/cw/kniffel/client/model/CellValue.java
@@ -0,0 +1,16 @@
+package de.fh_dortmund.cw.kniffel.client.model;
+
+/**
+ * 
+ * @author tbs
+ *
+ */
+public class CellValue {
+	
+	private Integer value;
+
+	public CellValue() {
+		// TODO Auto-generated constructor stub
+	}
+	
+}
